0e97f1499f796d811daa0198087666f3e8784737
shray kumar <shraykumar100@gmail.com>
2022-11-03 22:27:45 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Heading from "./components/Heading";
+import Text from "./components/Text";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Text/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.jsx
@@ -0,0 +1,10 @@
+import './Universal.css';
+const Heading = () => {
+    return (
+        <div>
+            <h1 className='centre-align heading'>About</h1>
+        </div>
+    );
+};
+
+export default Heading;
